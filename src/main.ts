735a9c516fd5551767b8abc1550899e26a171b26
alvarado0925 <alvarado0925@gmail.com>
2024-04-29 00:06:37 -0500

    Configura los DTOs para los Products

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Products - Microservice')
+    .setDescription('The Product API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,33 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  Min,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @ApiProperty({ description: 'The name of the product', example: 'Shoes' })
+  @IsString({ message: 'This is a custom message' })
+  @IsNotEmpty()
+  readonly name: string;
+
+  @ApiProperty({
+    description: 'The description of the product',
+    example: 'This is a pair of shoes',
+  })
+  @IsNotEmpty()
+  readonly description: string;
+
+  @ApiProperty({ description: 'The price of the product', example: 100_000 })
+  @IsNumber({ maxDecimalPlaces: 4 })
+  @IsPositive()
+  @IsNotEmpty()
+  readonly price: number;
+
+  @ApiProperty({ description: 'The stock of the product', example: 100 })
+  @Min(0)
+  @IsNotEmpty()
+  readonly stock: number;
+}
