472f346b74d3271aed9b66a5105e27bfb96fbda7
alvarado0925 <alvarado0925@gmail.com>
2024-04-29 23:14:58 -0500

    Implementa el servicio de creación de Products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -13,7 +13,7 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
   }
 
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    return this.product.create({ data: createProductDto });
   }
 
   findAll() {
