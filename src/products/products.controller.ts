e52905ebae160702812eec1c446512a2ff62f709
alvarado0925 <alvarado0925@gmail.com>
2024-04-30 23:39:15 -0500

    Implementa búsqueda de productos por ID

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -4,11 +4,12 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   Query,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -20,26 +21,32 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new product' })
+  @ApiCreatedResponse({ description: 'Product created successfully' })
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'List all products' })
   findAll(@Query() pagination: PaginationDto) {
     return this.productsService.findAll(pagination);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+  @ApiOperation({ summary: 'Get a product by ID' })
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a product by ID' })
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a product by ID' })
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
   }
